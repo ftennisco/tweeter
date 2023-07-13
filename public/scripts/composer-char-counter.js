0d4393019395372234127c285b3cb23d83d40b85
ftennisco <f.tennisco@hotmail.com>
2023-07-13 13:45:16 -0700

    first commit of tweeter project, create webpage to post tweets.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("#tweet-text").on("input", function(event){
+    let tweetLength = this.value.length;
+    let charactersLeft = 140 - tweetLength;
+    console.log(charactersLeft);
+    
+    // Update the counter on the page
+    let counterElement = $(this).closest(".new-tweet").find(".counter");
+    counterElement.text(charactersLeft);
+
+    // Add or remove CSS class based on character limit
+    if (charactersLeft < 0) {
+      counterElement.addClass("invalid");
+    } else {
+      counterElement.removeClass("invalid");
+    }
+  });
+
+  console.log("DOM is ready!");
+
+});
