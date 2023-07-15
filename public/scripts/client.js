ff54b75b3d848e2cdee83489b2641fdf20344df6
ftennisco <f.tennisco@hotmail.com>
2023-07-15 15:35:50 -0700

    tweets refresh on submition, icons and timestamp appear correctly.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,11 +3,42 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready(function() {
+$(document).ready(function () {
+  $('form').submit(function (event) {
+    event.preventDefault();
 
-function createTweetElement(tweet) {
-  // Create the DOM elements for the tweet structure
-  const $tweet = $(`
+    const $form = $(this);
+    const formData = $form.serialize();
+
+    // Perform form data validation
+    const tweetText = $form.find('#tweet-text').val();
+    if (!tweetText) {
+      alert('Error: Tweet content cannot be empty.'); // Display error message for empty tweet
+      return false; // Exit the submit handler without submitting the form
+    }
+    if (tweetText.length > 140) {
+      alert('Error: Tweet content exceeds 140 chatacters.'); // Display error message for exceeding character limit
+      return false; // Exit the submit handler without submitting the form
+    }
+
+    // Send the serialized data to the server
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: formData,
+      success: function (response) {
+        console.log(response);
+        loadTweets();
+      },
+      error: function (xhr, status, error) {
+        console.log(error);
+      }
+    });
+  });
+
+  function createTweetElement(tweet) {
+    // Create the DOM elements for the tweet structure
+    const $tweet = $(`
   <article class="tweet">
     <header>
       <div class="tweet-header">
@@ -18,53 +49,46 @@ function createTweetElement(tweet) {
     </header>
     <p class="tweet-text">${tweet.content.text}</p>
     <footer class="tweet-footer">
-      <div>${tweet.created_at}</div>
+    <span> ${timeago.format(tweet.created_at)} </span>
       <div>
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-arrows-rotate"></i>
-        <i class="fa-solid fa-heart"></i>
+      <i class="fa-solid fa-flag"></i>
+      <i class="fa-solid fa-arrows-rotate"></i>
+      <i class="fa-solid fa-heart"></i>
       </div>
     </footer>
   </article>
 `);
 
-  // Return the tweet element
-  return $tweet;
-}
-
-// Create tweet data
-const tweetData = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Elon",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirElon"
-    },
-    "content": {
-      "text": "I am the king of Tesla."
-    },
-    "created_at": 1621116232227
+    // Return the tweet element
+    return $tweet;
   }
-]
 
-const renderTweets = function(tweets) {
-  for (const tweet of tweets) {
-    const $tweet = createTweetElement(tweet)
-    $('.tweet-container').append($tweet)
+
+  const renderTweets = function (tweets) {
+    //clear the tweets container first
+    $('.tweet-container').empty();
+    for (const tweet of tweets) {
+      const $tweet = createTweetElement(tweet)
+      $('.tweet-container').prepend($tweet)
+    }
+  };
+
+  function loadTweets() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json',
+      success: function (response) {
+        console.log(response);
+        renderTweets(response); // Call the renderTweets function with the received tweets
+      },
+      error: function (xhr, status, error) {
+        console.log(error); // Handle any errors that occured during the request 
+      }
+    });
   }
-}
-renderTweets(tweetData);
 
+  loadTweets();
 
+  
 });
